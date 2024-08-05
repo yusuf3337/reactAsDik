fd93db46385126686e362862360d68dbf4964daf
Yusuf Çepni <myusufcepni2@gmail.com>
2024-08-05 21:14:14 +0300

    agaBune

--- App.js
+++ App.js
@@ -6,6 +6,11 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+    <View style={styles.subContainer}>
+      <Text style={styles.textStyle}>Test Text</Text>
+    </View>
+
     </View>
   );
 }
@@ -17,4 +22,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  subContainer:{
+    width: 200,
+    height: 150,
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius: 25,
+    borderWidth:10,
+    borderColor:"red"
+  },
+  textStyle: {
+    color:'black',
+    fontWeight:'bold',
+    fontSize:32
+  }
 });
